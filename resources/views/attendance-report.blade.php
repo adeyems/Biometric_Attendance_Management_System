eaf0a969adcdef98263568b47e4b8ddde8ca0947
Adeyemo Qudus <qudus.adeyemo@supermartng.com>
2019-08-06 22:54:45 +0100

    chage a title

--- resources/views/attendance-report.blade.php
+++ resources/views/attendance-report.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header text-center">{{ __('Student Daily Attendance') }}</div>
+                    <div class="card-header text-center">{{ __('Student Attendance Report') }}</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('create-request-report') }}">
