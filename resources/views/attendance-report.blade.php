3a2b844d5ec3db8a4ae8a3a69538bcac5bf857f1
Qudus <adeyems@gmail.com>
2019-08-27 12:52:01 +0100

    fixes

--- resources/views/attendance-report.blade.php
+++ resources/views/attendance-report.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-header text-center">{{ __('Student Attendance Report') }}</div>
 
